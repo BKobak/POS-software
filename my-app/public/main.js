bfca2ec7f4af144c194333ac866615bf98ad8a59
BKobak <bkobak01@gmail.com>
2022-11-02 19:59:36 +0100

    React application using Electron

--- my-app/public/main.js
+++ my-app/public/main.js
@@ -8,14 +8,13 @@ require('@electron/remote/main').initialize()
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
     }
   })
 
+  win.maximize();
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
@@ -23,15 +22,12 @@ function createWindow() {
   )
 }
 
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow()
+})
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
