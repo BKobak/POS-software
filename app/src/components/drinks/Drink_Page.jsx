a7bacdb3d54f5d9b55b7ec0d8fd1087c51e03d4c
BKobak <bkobak01@gmail.com>
2023-01-17 18:37:25 +0100

    Drinks Page

--- app/src/components/drinks/Drink_Page.jsx
+++ app/src/components/drinks/Drink_Page.jsx
@@ -1,62 +1,48 @@
 import React from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+
 
 function Drink_Page () {
     return(
-        <div className='clear'>
-            {/* First category */}
-            <div className='menu-cols clear'>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-            </div>
+    <div>
+        <Table celled>
+            <Table.Header >
+                <Table.Row className='Header'>
+                    <Table.HeaderCell>Espresso</Table.HeaderCell>
+                    <Table.HeaderCell>Frappucino</Table.HeaderCell>
+                    <Table.HeaderCell>Brewed</Table.HeaderCell>
+                    <Table.HeaderCell>Tea</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
 
-            {/* Second category */}
-            <div className='menu-cols clear'>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-            </div>
-
-            {/* Third category */}
-            <div className='menu-cols clear'>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-
-            </div>
-
-            {/* Fourth category */}
-            <div className='menu-cols clear'>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-
-            </div>
-
-            {/* Fifth category */}
-            <div className='menu-cols clear'>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-                <button className='display-button menu-btn'>Latte</button>
-
-            </div>
-
-        </div>
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                    <Table.Cell><button className='display-button menu-btn'>cell</button></Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>
+    </div>
     )
 }
 export default Drink_Page;
